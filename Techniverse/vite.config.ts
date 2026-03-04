db7eeab47b3cad2018458793daa4eac7929dd4f1
Anshuman <anshumanseoczar@gmail.com>
2026-03-04 23:57:33 +0530

    Update vite.config.ts

--- Techniverse/vite.config.ts
+++ Techniverse/vite.config.ts
@@ -1,7 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
 
+export default defineConfig({
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    allowedHosts: ["techniverse.onrender.com"]
+  }
+});
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
